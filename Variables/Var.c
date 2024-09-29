528bad7a1e08b2ed11495d18916a8f7738035954
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-30 00:17:23 +0600

    Variable

---
+++ Variables/Var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    char c = 'A';
+    float pi = 3.14;
+    printf("%d\n", a);
+    printf("%c\n", c);
+    printf("%f\n", pi);
+    return 0;
+}
