6ea972d7cd6fcdccd57a715b56d454727fd951c2
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-30 02:44:19 +0600

    Data Types

---
+++ Variables_DataTypes/DataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10; // Integer
+    float height = 6.5;  // float 
+    char hash = '#';  // Character
+    char ch = 'A';
+    printf("The number is: %d\n", num);
+    printf("This is: %c\n", ch);
+    printf("Theaa selected Character is: %c\n", hash);
+    printf("Height: %f", height); printf(" inch\n");
+    return 0;
+}
