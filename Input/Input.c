509e8f9c0b4b762feccfbb01378c1795916193c6
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-02 02:22:46 +0600

    User Input & other fixes

---
+++ Input/Input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");                                 
+    scanf("%d", &num);
+    printf("The Number is: %d", num);
+    return 0;
+}
