eecb9b9d2d53c5d80b8725ec8ded4b123c04a769
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-02 10:30:58 +0600

    Escape Sequence

---
+++ Basics/EscapeSequence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    printf("\aAlert!!!\n");
+    printf("Prantik :\tMy name is \"Prantik.\" What\'s yours\?\nStranger:\tI'm Stranger. Thx, Bai.\n");
+    // getch();
+    return 0;
+}
