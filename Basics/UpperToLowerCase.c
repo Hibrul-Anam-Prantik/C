96ab5fa1230324bc172ff21537afcce2e6903dfc
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-02 18:01:48 +0600

    UpperCase to LowerCase conversion

---
+++ Basics/UpperToLowerCase.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main() {
+    printf("Enter a UpperCase Letter: ");
+    char ch; 
+    scanf("%c", &ch);
+    // way - 1
+    int ascii = ch + 32;
+    ch = (char) ascii;
+    printf("The LowerCase Letter is: %c\n", ch);
+    // way - 2
+    printf("Enter a UpperCase Letter: ");
+    scanf(" %c", &ch);      // 1 space to consume the previous scanf's new line '\n'
+    printf("The LowerCase Letter is: %c\n", ((int) ch + 32));
+    // way - 3
+    printf("Enter a UpperCase Letter: ");
+    scanf(" %c", &ch);      // 1 space to consume the previous scanf's new line '\n'
+    printf("The LowerCase Letter is: %c\n", (ch + 32)); 
+    return 0;
+}
