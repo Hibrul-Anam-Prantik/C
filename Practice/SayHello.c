1a758f7a06fdf046364b4680bc9dba3b9bc410e0
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-03 23:34:22 +0600

    Practice C language(input, concatenation, printing)

---
+++ Practice/SayHello.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+    printf("Hello!\n => Hello!\n");
+    int n1, n2;
+    printf("Calculating Summation of two integer numbers(x, y)-\n x = ");
+    scanf("%d", &n1);
+    printf(" y = ");
+    scanf("%d", &n2);
+    printf("Summation = %d", (n1 + n2));
+    return 0;
+}
